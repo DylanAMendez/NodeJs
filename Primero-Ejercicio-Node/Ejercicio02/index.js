526a736accf387b280429a67557f7f6ce48e91a8
Dylan <100449253+DylanAMendez@users.noreply.github.com>
2022-10-10 16:38:29 -0300

    Add files via upload

---
+++ Primero-Ejercicio-Node/Ejercicio02/index.js
@@ -0,0 +1,11 @@
+let nombre = "Dylan A. Mendez";
+let edad = 22;
+// ¿Eres desarrollador?
+true; 
+new Date(2000, 1, 01);
+const libroFavorito ={
+    titulo: "Divina Comedia",
+    autor: "Dante Alighieri",
+    fecha: 1321,
+    url: "https://es.wikipedia.org/wiki/Divina_comedia"
+};
