b4811e5d66bb9d95b1e9c10fdf4be587d3a1de6d
Dylan <100449253+DylanAMendez@users.noreply.github.com>
2022-10-18 20:33:11 -0300

    Update ejercicio05.js

--- Primero-Ejercicio-Node/Ejercicio05/ejercicio05.js
+++ Primero-Ejercicio-Node/Ejercicio05/ejercicio05.js
@@ -3,9 +3,12 @@ let altura_m = 1.65;
 let peso_kg = 62.5;
 
 let altura_redondeada = Math.round((altura_m * 100) /100);
-
+let altura_r = Math.ceil(altura_m);
+console.log(altura_r)
 console.log(altura_redondeada);
-console.log(parseInt(altura_m));
 
-let maxMin = Number.MAX_VALUE;
-console.log(maxMin + 1)
+let peso_r = Math.floor(peso_kg);
+console.log(peso_r);
+
+let maxMin = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(maxMin);
