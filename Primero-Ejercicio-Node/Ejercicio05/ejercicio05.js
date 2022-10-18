270c6895fdd53136b5598874bac4726122099164
Dylan <100449253+DylanAMendez@users.noreply.github.com>
2022-10-18 20:29:09 -0300

    Add files via upload

---
+++ Primero-Ejercicio-Node/Ejercicio05/ejercicio05.js
@@ -0,0 +1,11 @@
+let altura_cm = 165;
+let altura_m = 1.65;
+let peso_kg = 62.5;
+
+let altura_redondeada = Math.round((altura_m * 100) /100);
+
+console.log(altura_redondeada);
+console.log(parseInt(altura_m));
+
+let maxMin = Number.MAX_VALUE;
+console.log(maxMin + 1)
