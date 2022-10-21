55845a5c742e3a05ed166447188c729b23595d4e
Dylan <100449253+DylanAMendez@users.noreply.github.com>
2022-10-21 15:57:54 -0300

    Add files via upload

---
+++ Primero-Ejercicio-Node/Ejercicio08/ejercicio08.js
@@ -0,0 +1,19 @@
+function siempreTrue(){
+    if (true){
+        return true
+    } else {
+        return false 
+    }
+}
+
+console.log(siempreTrue())
+
+const asincrona = new Promise(() => {
+    setTimeout [console.log("Hola soy una promesa")] [(50000)]}
+)
+
+
+
+
+
+
