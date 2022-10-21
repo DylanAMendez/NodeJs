281c9e5bc9d073c22fd6cd975be62058e111751f
Dylan <100449253+DylanAMendez@users.noreply.github.com>
2022-10-21 12:12:04 -0300

    Add files via upload

---
+++ Primero-Ejercicio-Node/Ejercicio07/conjuntos.js
@@ -0,0 +1,8 @@
+const familia = ["Ant Merc", "Jua Men", "Lea Men", "Dylan Men"]; 
+const familia_set = new Set(familia);
+console.log(familia_set)
+
+familia_set.add("Dylan Men");
+console.log(familia_set)
+familia_set.add("Javascript");
+console.log(familia_set)
