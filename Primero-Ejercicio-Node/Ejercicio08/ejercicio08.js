b259539389ade89e1047eee2146ccd5df5d32630
Dylan <100449253+DylanAMendez@users.noreply.github.com>
2022-10-21 15:59:13 -0300

    Update ejercicio08.js

--- Primero-Ejercicio-Node/Ejercicio08/ejercicio08.js
+++ Primero-Ejercicio-Node/Ejercicio08/ejercicio08.js
@@ -1,19 +1,14 @@
-function siempreTrue(){
-    if (true){
-        return true
-    } else {
-        return false 
-    }
+function devuelveTrue() {
+    return true
 }
 
-console.log(siempreTrue())
+async function damePromesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
 
-const asincrona = new Promise(() => {
-    setTimeout [console.log("Hola soy una promesa")] [(50000)]}
-)
-
-
-
-
-
-
+function* idsPares() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
